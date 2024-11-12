e6cb5e5092a48a335a9f855884fb84078d077bd7
saiffardin <saiffardin@gmail.com>
2024-11-13 00:57:00 +0600

    add - tailwind || vite-plugin-checker || sass || prettier || dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import dotenv from "dotenv";
+import { resolve } from "path";
+import checker from "vite-plugin-checker";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Load env file based on the mode
+  const env = loadEnv(mode, process.cwd());
+  dotenv.config({ path: resolve(process.cwd(), `.env.${mode}`) });
+
+  return {
+    server: {
+      port: 3000,
+    },
+
+    plugins: [react(), checker({ typescript: true })],
+
+    define: {
+      "process.env": env,
+    },
+
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
+    },
+  };
+});
